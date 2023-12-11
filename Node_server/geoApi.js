d4f6d051c381e310ec2b42f5c50dbba50900b10a
Pratik Pimpalkar <pratik@xerocodee.com>
2023-12-11 11:15:46 +0530

    Optimized tour

--- Node_server/geoApi.js
+++ Node_server/geoApi.js
@@ -16,14 +16,14 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
     return distance;
 }
 
-function buildSortedGraph() {
+function buildSortedGraph(data) {
     const graph = {};
-    testData.forEach((node1, index1) => {
+    data.forEach((node1, index1) => {
         graph[node1.name] = {
             data: node1,
             edges: {},
         };
-        testData.forEach((node2, index2) => {
+        data.forEach((node2, index2) => {
             if (index1 !== index2) {
                 const distance = calculateDistance(
                     node1.lat,
@@ -45,82 +45,68 @@ function buildSortedGraph() {
             return acc;
         }, {});
     });
+    // console.dir(graph["Stable temple (krishna gujarati temple)"]);
     return graph;
 }
 
-function generateOptimalTour(startingPointLat, startingPointLon, interests) {
-    const sortedGraph = buildSortedGraph();
-    const tourPlan = [];
-    const visitedInterests = {};
+const userInterests = [
+    "accomodation",
+    "commercial",
+    "catering",
+    "entertainment",
+    "national_park",
+    "beach",
+    "tourism",
+    "building.tourism",
+    "religion",
+    "sport",
+];
 
-    // Helper function to check if all interests are covered
-    function allInterestsCovered() {
-        return Object.keys(placeType).every((interest) => visitedInterests[interest]);
-    }
+// Function to check if a node has any of the specified interests
+function hasInterest(node, interests) {
+    return node.categories.some(category => interests.includes(category));
+}
+
+function generateOptimalTour(data, startingPointLat, startingPointLon, interests) {
+    const sortedGraph = buildSortedGraph(data);
+    const tourPlan = [];
+    const visitedNodes = new Set();
 
     let currentNode = null;
 
-    // Find the nearest node to the starting point that matches user interests
+    // Find the nearest node to the starting point that matches the user interests
     let minDistance = Infinity;
     for (const nodeName in sortedGraph) {
         const node = sortedGraph[nodeName];
-        const distance = calculateDistance(
-            startingPointLat,
-            startingPointLon,
-            node.data.lat,
-            node.data.lon
-        );
-
-        // Check if the place belongs to any of the specified interests
-        const interestCategories = node.data.categories.join(",");
-        if (
-            interests.some((interest) => interestCategories.includes(placeType[interest])) &&
-            distance < minDistance
-        ) {
-            minDistance = distance;
-            currentNode = node;
+        if (hasInterest(node.data, interests)) {
+            const distance = calculateDistance(
+                startingPointLat,
+                startingPointLon,
+                node.data.lat,
+                node.data.lon
+            );
+            if (distance < minDistance) {
+                minDistance = distance;
+                currentNode = node;
+            }
         }
     }
 
-    // Perform greedy algorithm to generate the tour
-    while (currentNode && !allInterestsCovered()) {
+    while (currentNode) {
         const { data } = currentNode;
-
-        // Check if the place belongs to any of the specified interests
-        Object.entries(placeType).forEach(([interest, categories]) => {
-            const interestCategories = categories.split(",");
-            if (
-                interestCategories.some((category) =>
-                    data.categories.includes(category)
-                ) &&
-                !visitedInterests[interest]
-            ) {
-                // For restaurants, add up to 2 places with the same interest
-                if (interest === 'restaurant') {
-                    if (!visitedInterests[interest]) {
-                        visitedInterests[interest] = true;
-                        tourPlan.push({ ...data });
-                    } else if (tourPlan.filter(place => place.categories.includes('catering')).length < 2) {
-                        tourPlan.push({ ...data });
-                    }
-                } else {
-                    visitedInterests[interest] = true;
-                    tourPlan.push({ ...data });
-                }
-            }
-        });
-
-        delete sortedGraph[currentNode.data.name]; // Mark node as visited
+        tourPlan.push({ ...data, edges: undefined });
+        visitedNodes.add(currentNode.data.name);
 
         let nextNode = null;
         let minEdgeDistance = Infinity;
 
-        // Find the nearest unvisited neighbor
+        // Find the nearest unvisited neighbor that matches the user interests
         for (const neighborName in currentNode.edges) {
             const neighbor = currentNode.edges[neighborName];
             if (
-                sortedGraph[neighbor.data.name] &&
-                neighbor.distance < minEdgeDistance
+                !visitedNodes.has(neighbor.data.name) &&
+                neighbor.distance < minEdgeDistance &&
+                hasInterest(neighbor.data, interests)
             ) {
                 minEdgeDistance = neighbor.distance;
                 nextNode = sortedGraph[neighbor.data.name];
@@ -132,7 +118,6 @@ function generateOptimalTour(startingPointLat, startingPointLon, interests) {
 
     return tourPlan;
 }
-
 
 const placeType = {
     hotels: "accomodation",
@@ -147,38 +132,39 @@ const placeType = {
 
 const generateTourAPI = async (req, res) => {
     try {
-        // let lng = 79.06031117301508
-        // let lat = 21.179601416524175
-        // let categoriesList = req.body.categoriesList;
-        // console.log(categoriesList);
-        // const apiUrl = 'https://api.geoapify.com/v2/places';
-        // let categories = categoriesList.map(category => placeType[category]).join(',');
-        // let filter = `circle:${lng},${lat},100000`;
-        // let bias = `proximity:${lng},${lat}`;
-        // let limit = 5;
-        // let apiKey = '6ad67642e2f94ad99d6fc359677c6706';
-        // let params = {
-        //     categories,
-        //     filter,
-        //     bias,
-        //     limit,
-        // };
-        // const headers = {
-        //     'Authorization': apiKey
-        // };
-        // const { data } = await axios.get(apiUrl, { params, headers })
-        // const extractedData = extractPropertiesForList(data.features, propertiesToExtract)
-        // return res.json(extractedData)
 
-        const startingPointLat = 21.179581408263445;
-        const startingPointLon = 79.0602468;
-        const interests = ["tourism", "restaurant"]
-        const tourPlan = generateOptimalTour(startingPointLat, startingPointLon, interests);
+        let lng = req.body.lng
+        let lat = req.body.lat
+        let categoriesList = req.body.categoriesList;
+        const apiUrl = 'https://api.geoapify.com/v2/places';
+        let categories = categoriesList.map(category => category).join(',');
+        console.log(categories);
+        let filter = `circle:${lng},${lat},500000`;
+        // let bias = `proximity:${lng},${lat}`;
+        let limit = 10;
+        let apiKey = '6ad67642e2f94ad99d6fc359677c6706';
+        let params = {
+            categories,
+            filter,
+            // bias,
+            limit,
+        };
+        const headers = {
+            'Authorization': apiKey
+        };
+        const { data } = await axios.get(apiUrl, { params, headers })
+        const extractedData = extractPropertiesForList(data.features, propertiesToExtract)
+        // return res.json(extractedData)
+        const startingPointLat = req.body.lat;
+        const startingPointLon = req.body.lng;
+        const maxDistancePerDay = req.body.maxDistancePerDay;
+        const interests = categories
+        const tourPlan = generateOptimalTour(extractedData, startingPointLat, startingPointLon, interests, maxDistancePerDay);
         console.log("Optimal Tour Plan:", tourPlan);
         return res.json(tourPlan)
 
     } catch (error) {
-        console.log(error);
+        // console.log(error);
         return res.json({ message: "true" })
     }
 }
